b0c96564491d4951a56612d0cfacdc6ab0f3ba1d
Riku Putkinen <putkinenriku@gmail.com>
2022-09-14 17:16:51 +0300

    Make pixels change color when hovered over

--- script.js
+++ script.js
@@ -6,14 +6,34 @@ function createCanvas() {
   }
 
   const area = size ** 2;
+  
   const drawingArea = document.querySelector('#drawingArea');
+  if (drawingArea.hasChildNodes()) {
+    while (drawingArea.firstChild) {
+      drawingArea.removeChild(drawingArea.firstChild);
+    }
+  }
+
+  const canvas = document.createElement('div');
+  canvas.setAttribute('id', 'canvas');
+  drawingArea.appendChild(canvas);
 
   for (let i = 1; i <= area; i++) {
     const pixel = document.createElement('div');
     pixel.classList.add('pixel');
-    drawingArea.appendChild(pixel);
+    canvas.appendChild(pixel);
     console.log(`div ${i} created`);
   }
+
+  canvas.setAttribute('style', `grid-template-columns: repeat(${size}, 1fr); grid-auto-rows: 1fr`);
+
+  const pixels = document.querySelectorAll('.pixel');
+  pixels.forEach(pixel => {
+    pixel.addEventListener('mouseenter', () => {
+      pixel.style.backgroundColor = 'black';
+      console.log('event');
+    })
+  })
 }
 
 const button = document.querySelector('button#sizeSet');
